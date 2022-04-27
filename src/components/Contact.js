60785dbbad0ebde9ba2ac3e756d2ad69437ab3ee
Gerardo <gerardoluisbv@gmail.com>
2022-04-27 15:40:07 -0500

    fix component Contact CI/CD

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,16 +3,15 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="home animate__animated animate__fadeIn animate__delay-0.8s">
-      <div className="container_home homeImage">
-        <div className="content_about">
-          <div className="title_home">
-            <h1> Contacto </h1>
-          </div>
-
-          <hr />
+    <div className="container_home homeImage">
+      <div className="content_about">
+        <div className="title_home">
+          <h1> Contact </h1>
         </div>
+        <hr />
       </div>
     </div>
+  </div>
   );
 };
 
